daae7503456bd15b0f8b18129f3bca06e358ddb2
Carlos Pisano <carlospisano@mywtm.com>
2023-03-23 19:29:58 +1300

    add Products and Product components

--- client/my-store/src/pages/product/Product.jsx
+++ client/my-store/src/pages/product/Product.jsx
@@ -1,9 +1,66 @@
 import "./Product.scss";
+import {useState} from "react";
+import {AddShoppingCart, Balance, FavoriteBorder, FavoriteBorderOutlined} from "@mui/icons-material";
+import {Link} from "react-router-dom";
 
 const Product = () => {
+
+    const [selectedImage, setSelectedImage] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+
+
+
+    const images = [
+        "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    ]
+
     return (
-        <div className="product"> Product </div>
+        <div className="product">
+            <div className="left">
+                <div className="images">
+                    <img src={images[0]} alt=""  onClick={e => setSelectedImage(0)}/>
+                    <img src={images[1]} alt="" onClick={e => setSelectedImage(1)}/>
+                </div>
+                <div className="mainImg">
+                    <img src={images[selectedImage]} alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <h1>Title</h1>
+                <span className="price">$200</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur deleniti dolores eos iusto maiores nam non porro saepe vel voluptate. Aliquid, cum, minus? Dicta eum nihil numquam optio quod unde!</p>
+                <div className="quantity">
+                    <button onClick={() => setQuantity(prevState => prevState === 1 ? 1 :  prevState - 1)}>-</button>
+                    <span>{quantity}</span>
+                    <button onClick={() => setQuantity(prevState => prevState + 1)}>+</button>
+                </div>
+                <button className="add">
+                    <AddShoppingCart/> ADD TO CART
+                </button>
+                <div className="links">
+                    <div className="item">
+                        <FavoriteBorder/> ADD TO WISH LIST
+                    </div>
+                    <div className="item">
+                        <Balance/> ADD TO COMPARE
+                    </div>
+                </div>
+                <div className="info">
+                    <span>Vendor: Polo</span>
+                    <span>Product Typw: T-Shirt</span>
+                    <span>Tag: T-Shirt, Women, Top</span>
+                </div>
+                <hr/>
+                <div className="info">
+                    <span>DESCRIPTION</span>
+                    <hr/>
+                    <span>ADDITIONAL INFORMATION</span>
+                    <hr/>
+                    <span>FAQ</span>
+                </div>
+            </div>
+        </div>
     )
 }
-
 export default Product
--- client/my-store/src/pages/products/Products.jsx
+++ client/my-store/src/pages/products/Products.jsx
@@ -1,8 +1,66 @@
 import "./Products.scss";
+import List from "../../components/list/List";
+import {useParams} from "react-router-dom";
+import {useState} from "react";
 
 const Products = () => {
+
+    const CatId = parseInt(useParams().id);
+    const [maxPrice, setMaxPrice] = useState(1000);
+    const [sort, setSort] = useState("asc");
+
+
     return (
-        <div className="products"> Products </div>
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem checkboxItems">
+                        <div className="checkboxContainer">
+                            <input type="checkbox" id="1" value={1}/>
+                            <label htmlFor="1">Shoes</label>
+                        </div>
+                        <div className="checkboxContainer">
+                            <input type="checkbox" id="2" value={2}/>
+                            <label htmlFor="2">Coats</label>
+                        </div>
+                        <div className="checkboxContainer">
+                            <input type="checkbox" id="3" value={3}/>
+                            <label htmlFor="3">Skirts</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)}/>
+                        <span>{maxPrice}</span>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <div className="radioContainer">
+                            <input type="radio" id="asc" value="asc" name="price"
+                                   onChange={(e) => setSort(e.target.value)}/>
+                            <label htmlFor="asc">Price (Lowest First)</label>
+                        </div>
+                        <div className="radioContainer">
+                            <input type="radio" id="desc" value="desc" name="price"
+                                   onChange={(e) => setSort(e.target.value)}/>
+                            <label htmlFor="desc">Price (Highest First)</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right">
+                <img className="catImg"
+                     src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                     alt=""/>
+                <List catId={CatId} maxPrice={maxPrice} sort={sort}/>
+            </div>
+        </div>
     )
 }
 
